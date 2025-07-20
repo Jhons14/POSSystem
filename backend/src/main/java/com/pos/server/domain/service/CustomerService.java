2ea406aa77b379e00b395f87311bc36102b6df7b
Jhons14 <tivenorjuela7@gmail.com>
2025-07-20 11:35:06 -0500

    Adding password bcrypt encryption

--- backend/src/main/java/com/pos/server/domain/service/CustomerService.java
+++ backend/src/main/java/com/pos/server/domain/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.pos.server.domain.service;
 import com.pos.server.domain.Customer;
 import com.pos.server.domain.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,12 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Customer save(Customer customer) {
+        String encryptedPassword =  passwordEncoder.encode(customer.getPassword());
+        customer.setPassword(encryptedPassword);
         return customerRepository.save(customer);
     }
 }
--- backend/src/main/java/com/pos/server/web/security/SecurityConfig.java
+++ backend/src/main/java/com/pos/server/web/security/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -88,6 +90,9 @@ public class SecurityConfig   {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
 
